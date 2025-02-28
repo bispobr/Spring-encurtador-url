4a454c4b2d2ba91a29581c6233ebd0fbb7dc9028
bispobr <156684382+bispobr@users.noreply.github.com>
2025-02-28 14:31:22 -0300

    feat: aiciona metodo gerador de url curta

---
+++ src/main/java/com/site/url/services/UrlService.java
@@ -0,0 +1,56 @@
+package com.site.url.services;
+
+import com.site.url.model.Url;
+import com.site.url.repository.UrlRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.Optional;
+
+@Service
+public class UrlService {
+
+    @Autowired
+    private UrlRepository urlRepository;
+
+    public String urlCurta(String urlLonga){
+        String urlencurtada = geradorUrlcurta(urlLonga);
+        Url url = new Url();
+        url.setUrlLonga(urlLonga);
+        url.setUrlCurta(urlencurtada);
+        url.setDataExpiracao(LocalDateTime.now().plusDays(10));
+        urlRepository.save(url);
+        return urlencurtada;
+    }
+
+
+    private String geradorUrlcurta(String urlLonga){
+        String BASE_URL = "https:/xxx.com/";
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(urlLonga.getBytes(StandardCharsets.UTF_8));
+            String encoded = Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
+            return encoded.substring(0, 8);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Erro ao gerar URL encurtada", e);
+        }
+    }
+
+    public Optional<Url>getUrlOriginal(String urlcurta){
+        Optional<Url> urlOptional = urlRepository.findByUrlCurta(urlcurta);
+        if (urlOptional.isPresent()){
+            Url url = urlOptional.get();
+            if (url.getDataExpiracao().isAfter(LocalDateTime.now())){
+                return Optional.of(url);
+            } else {
+                urlRepository.delete(url);
+            }
+        }
+        return Optional.empty();
+    }
+}
