b1b12b5cff0b676e8766d8b0bf0c1a28110b9454
bispobr <156684382+bispobr@users.noreply.github.com>
2025-09-14 09:58:15 -0300

    fix: coreção de  pequeno erro no código

--- src/main/java/com/site/url/model/Url.java
+++ src/main/java/com/site/url/model/Url.java
@@ -19,7 +19,7 @@ import java.time.LocalDateTime;
 public class Url {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String urlLonga;
     private String urlCurta;
     private LocalDateTime dataExpiracao;
