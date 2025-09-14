fc7af1672df0c22bd8e4f2bc3c720df3f916b234
bispobr <156684382+bispobr@users.noreply.github.com>
2025-09-14 10:06:29 -0300

    feat: add uso de mappers e logs

--- src/main/java/com/site/url/services/UrlService.java
+++ src/main/java/com/site/url/services/UrlService.java
@@ -1,11 +1,18 @@
 package com.site.url.services;
 
+import com.site.url.dto.UrlRequisicaoDTO;
+import com.site.url.dto.UrlRespostaDTO;
+import com.site.url.mapper.UrlMapper;
 import com.site.url.model.Url;
 import com.site.url.repository.UrlRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -20,46 +27,52 @@ public class UrlService {
     @Autowired
     private UrlRepository urlRepository;
 
-    public String urlCurtaMontagem(String urlLonga){
-        String urlencurtada = geradorUrlcurta(urlLonga);
-        Url url = new Url();
-        url.setUrlLonga(urlLonga);
-        url.setUrlCurta(urlencurtada);
-        url.setDataExpiracao(LocalDateTime.now().plusDays(10));
+    @Autowired
+    private UrlMapper mapper;
+
+    public UrlRespostaDTO urlCurtaMontagem(UrlRequisicaoDTO requisicao){
+
+        Url url = geradorUrlcurta(mapper.paraUrl(requisicao));
         urlRepository.save(url);
-        log.info("Montagem url concluida");
-        return urlencurtada;
+        log.info("Url encurtada Salva na Base de Dados");
+        return mapper.paraResposta(url);
     }
 
 
-    private String geradorUrlcurta(String urlLonga){
-        String BASE_URL = "https:/aaa.com/";
+    private Url geradorUrlcurta(Url url){
+        String BASE_URL = "https://aaa.com/";
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(urlLonga.getBytes(StandardCharsets.UTF_8));
+            byte[] hash = digest.digest(url.getUrlLonga().getBytes(StandardCharsets.UTF_8));
             String encoded = Base64.getUrlEncoder().withoutPadding().encodeToString(hash);
-            log.info("url encurtada retornada");
-            return encoded.substring(0, 8);
+            url.setUrlCurta(encoded.substring(0, 8));
+            url.setDataExpiracao(LocalDateTime.now().plusDays(10));
+            log.info("url encurtada gerada");
+            return url;
         } catch (NoSuchAlgorithmException e) {
             log.error("Erro ao gerar URL encurtada");
             throw new RuntimeException("Erro ao gerar URL encurtada", e);
         }
     }
 
-    public Optional<Url>getUrlOriginal(String urlcurta){
+    public ResponseEntity<Void> getUrlOriginal(String urlcurta){
 
         Optional<Url> urlOptional = urlRepository.findByUrlCurta(urlcurta);
         if (urlOptional.isPresent()){
             Url url = urlOptional.get();
             if (url.getDataExpiracao().isAfter(LocalDateTime.now())){
                 log.info("Url original retornada");
-                return Optional.of(url);
+                HttpHeaders headers =  new HttpHeaders();
+                headers.setLocation(URI.create(url.getUrlLonga()));
+                return ResponseEntity.status(HttpStatus.FOUND).headers(headers).build();
             } else {
                 log.info("Url expirada apagada");
                 urlRepository.delete(url);
             }
         }
+
         log.info("url vazia retornada");
-        return Optional.empty();
+        return  ResponseEntity.notFound().build();
     }
 }
+
