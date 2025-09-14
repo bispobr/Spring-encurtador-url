00e083e9bdc4b80709244b990a2fc5139d939895
bispobr <156684382+bispobr@users.noreply.github.com>
2025-09-14 10:09:24 -0300

    feat: adiciona  o uso de mapper

--- src/main/java/com/site/url/controller/UrlController.java
+++ src/main/java/com/site/url/controller/UrlController.java
@@ -1,57 +1,47 @@
 package com.site.url.controller;
 
-import com.site.url.model.Url;
+import com.site.url.dto.UrlRequisicaoDTO;
+import com.site.url.dto.UrlRespostaDTO;
+import com.site.url.mapper.UrlMapper;
 import com.site.url.services.UrlService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.net.URI;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 @Slf4j
 @RestController
-@RequestMapping("/url")
 public class UrlController {
 
     @Autowired
     private UrlService urlService;
 
+    @Autowired
+    private UrlMapper mapper;
+
     @PostMapping("/shorten-url")
     @Operation(description = "Endpoint responsável por receber url para ser encurtada")
-    @ApiResponse(responseCode = "200", description = "Listagem bem sucedida")
+    @ApiResponse(responseCode = "200", description = "Encurtamento bem sucedido")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
     @ApiResponse(responseCode = "500", description = "Erro interno")
-    public ResponseEntity<Map<String,String>> urlcurta(@RequestBody Map <String,String> request){
+    public ResponseEntity<UrlRespostaDTO> urlcurta(@RequestBody @Valid UrlRequisicaoDTO request, HttpServletRequest servletRequest){
         log.info("Solicitação para encurtar url recebida");
-        String UrlLonga =  request.get("url");
-        String urlencurtada =  urlService.urlCurtaMontagem(UrlLonga);
-        Map<String,String> resposta =  new HashMap<String,String>();
-        resposta.put("url","https://xxx.com/"+urlencurtada);
-
-        return ResponseEntity.ok(resposta);
+        String urlRedirecionada = servletRequest.getRequestURL().toString().replace("shorten-url", urlService.urlCurtaMontagem(request).urlCurta());
+        return ResponseEntity.ok( mapper.paraResposta(urlRedirecionada));
     }
 
     @GetMapping ("/{urlcurta}")
-    @Operation(description = "Endpoint responsável por retornar url original")
-    @ApiResponse (responseCode = "200", description = "Listagem bem sucedida")
-    @ApiResponse (responseCode = "404", description = "Url não encontrada")
-    public ResponseEntity<Object> getUrlEncurtada(@PathVariable String urlcurta){
+    @Operation(description = "Endpoint responsável por retornar url original, (Realize a chamada atraves do navegador)")
+    @ApiResponse (responseCode = "302", description = "URL encontrada e válida")
+    @ApiResponse(responseCode = "400", description = "Erro de Requisição")
+    @ApiResponse (responseCode = "404", description = "URL não encontrada ou expirada")
+    public ResponseEntity<Void> getUrlEncurtada(@PathVariable String urlcurta){
         log.info("solicitação para retornar url original recebida");
-        Optional<Url> urlOptional = urlService.getUrlOriginal(urlcurta);
-
-        if (urlOptional.isPresent()){
-            Url url = urlOptional.get();
-            System.out.println("Redirecionando para" +url.getUrlLonga());
-            return ResponseEntity.status(200).location(URI.create(url.getUrlLonga())).build();
-
-        }
-        System.out.println("URL expirada ou não encontrada " + urlcurta);
-        log.info("url solicitada expirada ou não encontrada");
-        return ResponseEntity.notFound().build();
+        return urlService.getUrlOriginal(urlcurta);
     }
 
 }
